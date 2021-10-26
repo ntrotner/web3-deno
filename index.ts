720a3c4e33fd6c1043dc91a952e1d5d393f2f644
Nikita Trotner <nikita@ttnr.me>
2021-10-26 21:18:48 +0200

    provisionally added types

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import "https://deno.land/x/xhr@0.1.0/mod.ts";
 import { Web3 } from './web3.min.js'
 
 const web3 = new Web3('http://85.214.129.69:8545')
-web3.eth.getAccounts(console.log)
+let accounts = await web3.eth.getAccounts()
+console.log(accounts)
 let balance = await web3.eth.getBalance('0x9f6c5B894fe87F2F0dD35F1B5501948Dd4C93f80')
 console.log(balance)
