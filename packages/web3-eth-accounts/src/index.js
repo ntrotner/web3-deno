cfdef4e1b21907c14504f979df6c96cc1d7ff150
Nikita Trotner <nikita@ttnr.me>
2021-11-01 17:44:24 +0100

    Update index.js

--- packages/web3-eth-accounts/src/index.js
+++ packages/web3-eth-accounts/src/index.js
@@ -25,11 +25,11 @@
 import core from 'https://github.com/ntrotner/web3-deno/raw/main/packages/web3-core/src/index.js';
 import Method from 'https://github.com/ntrotner/web3-deno/raw/main/packages/web3-core-method/src/index.js';
 import Account from 'https://jspm.dev/eth-lib/lib/account';
-var cryp = (typeof global === 'undefined') ? require('https://jspm.dev/crypto-browserify') : require('crypto');
+import crypt from 'https://jspm.dev/crypto-browserify';
 import scrypt from 'https://jspm.dev/scrypt-js';
-import uuid from 'https://jspm.dev/uuid';
+import * as uuid from 'https://jspm.dev/uuid';
 import utils from 'https://github.com/ntrotner/web3-deno/raw/main/packages/web3-utils/src/index.js';
-import { * as helpers} from 'https://github.com/ntrotner/web3-deno/raw/main/packages/web3-core-helpers/src/index.js';
+import * as helpers from 'https://github.com/ntrotner/web3-deno/raw/main/packages/web3-core-helpers/src/index.js';
 import TX from 'https://jspm.dev/@ethereumjs/tx'
 import Common from 'https://jspm.dev/@ethereumjs/common';
 import {Hardfork as HardForks} from 'https://jspm.dev/@ethereumjs/common';
