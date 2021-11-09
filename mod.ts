f211f99e4aef9f78b008d25da030b0d4e3ac36e3
Nikita Trotner <nikita@ttnr.me>
2021-11-09 15:46:32 +0100

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import Web3 from 'https://raw.githubusercontent.com/ntrotner/web3-deno/main/packages/web3/src/index.js';
+import Web3 from './packages/web3/src/index.js';
 
-export type * from 'https://raw.githubusercontent.com/ntrotner/web3-deno/main/packages/web3/types/index.d.ts';
+import Web3 from './packages/web3/types/index.d.ts'
 export default Web3;
