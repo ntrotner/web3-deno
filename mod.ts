3f53089291399c3e106f5590ea414851bab587c8
Nikita Trotner <nikita@ttnr.me>
2021-11-09 19:31:07 +0100

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-// @deno-types="./packages/web3/types/index.d.ts"
+/// <reference path="./packages/web3/types/index.d.ts" />
 import Web3 from './packages/web3/src/index.js';
 
 export default Web3;
