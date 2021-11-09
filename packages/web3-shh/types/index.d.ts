e360eb1115dbf777371703a958d24acd6490f24c
Nikita Trotner <nikita@ttnr.me>
2021-11-09 16:12:14 +0100

    Update index.d.ts

--- packages/web3-shh/types/index.d.ts
+++ packages/web3-shh/types/index.d.ts
@@ -17,7 +17,7 @@
  * @date 2018
  */
 
-import * as net from 'net';
+import * as net from 'https://jspm.dev/net';
 import {
   BatchRequest, provider, Providers, Extension, 
 } from 'https://raw.githubusercontent.com/ntrotner/web3-deno/main/packages//web3-core/types/index.d.ts';
